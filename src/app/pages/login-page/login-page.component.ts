08b1b50c91b907282a5d36c40a78f6c7dfd064aa
Fur1ouS <Fur1ouS@ya.ru>
2025-03-11 17:34:35 +0300

    Add authorization page and add form

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {AuthService} from "../../auth/auth.service";
+import {LoginPageAuthInterface} from "./interface/login-page-auth-interface";
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
+  authService = inject(AuthService)
 
+  form = new FormGroup({
+    username: new FormControl("", Validators.required),
+    password: new FormControl("", Validators.required),
+  })
+
+  onSubmit() {
+    if (this.form.valid) {
+      const username = this.form.get('username')!.value ?? '';
+      const password = this.form.get('password')!.value ?? '';
+
+      if (username.trim() === '' || password.trim() === '') {
+        return;
+      }
+
+      const loginData: LoginPageAuthInterface = {
+        username,
+        password
+      };
+      this.authService.login(loginData)
+        .subscribe(res => {
+          console.log(res)
+      });
+    }
+  }
 }
