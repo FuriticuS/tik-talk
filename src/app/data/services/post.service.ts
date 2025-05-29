580d53a88c0f36c9a6b21fba70b36a47de8c55fe
Fur1ouS <Fur1ouS@ya.ru>
2025-05-29 18:44:11 +0300

    Add on profile page-post-input and create post and load comments and length comments and add button show all comments

--- src/app/data/services/post.service.ts
+++ src/app/data/services/post.service.ts
@@ -1,7 +1,7 @@
 import {inject, Injectable, signal} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
-import {Post, PostCreateDto} from "../interfaces/post.interface";
-import {switchMap, tap} from "rxjs";
+import {CommentCreateDto, Post, PostComment, PostCreateDto} from "../interfaces/post.interface";
+import {map, Observable, switchMap, tap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +11,7 @@ export class PostService {
   private _baseApiUrl = 'https://icherniakov.ru/yt-course/';
   posts = signal<Post[]>([])
 
+  // запрос на созданный пост
   createPost(payload: PostCreateDto) {
     return this._http.post<Post>(
       `${this._baseApiUrl}post/`,
@@ -22,12 +23,26 @@ export class PostService {
     )
   }
 
-  fetchPosts() {
+  // показать все посты
+  fetchPosts(): Observable<Post[]> {
     return this._http.get<Post[]>(`${this._baseApiUrl}post/`)
       .pipe(
         tap((response: Post[]) => {
           this.posts.set(response)
         })
+      )
+  }
+
+  // запрос на созданный комментарий к посту
+  createComment(payload: CommentCreateDto): Observable<PostComment> {
+    return this._http.post<PostComment>(`${this._baseApiUrl}comment/`, payload)
+  }
+
+  // запрос на отображение коммента сразу после отправки
+  getCommentsByPostId(postId: number): Observable<PostComment[]> {
+    return this._http.get<Post>(`${this._baseApiUrl}post/${postId}`)
+      .pipe(
+        map((response: Post) => response.comments),
       )
   }
 }
